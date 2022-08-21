28cef984a2df195fc312cc28a2b6443173e5f9b3
PFC(Ian) <pfc-validator@protonmail.com>
2022-08-21 11:35:32 -0500

    remove old footer

--- pages/stats.tsx
+++ pages/stats.tsx
@@ -27,9 +27,6 @@ export default function StatsPage ({ network, chain}: SteakProps)  {
         <Text fontSize="2xl" fontWeight="800" opacity={0.4}>
           Under construction... More stats coming soon!
         </Text>
-        <Text fontSize="2xl" fontWeight="800" opacity={0.4}>
-          This site is being maintained by PFC with @Larry0x&apos;s consent, and will be handed over when he chooses
-        </Text>
       </Box>
     </>
   );
