c11c14313a079a5db4092663f2cff83925bab245
PFC(Ian) <pfc-validator@protonmail.com>
2022-08-25 16:14:31 -0500

    admin add/remove functions

--- modules/steak/Admin.tsx
+++ modules/steak/Admin.tsx
@@ -1,26 +1,55 @@
-import { Box, Flex, Text, Button, useDisclosure} from "@chakra-ui/react";
+import {Box, Flex, Text, Button, useDisclosure, Input, FormLabel} from "@chakra-ui/react";
 import {FC, useState} from "react";
 
 import Header from "../common/components/Header";
-import { useWallet} from "@wizard-ui/react";
+import {useWallet} from "@wizard-ui/react";
 import {SteakProps} from "../../pages/_app";
 import {EncodeObject} from "@cosmjs/proto-signing";
 import {MsgExecuteContract} from "cosmjs-types/cosmwasm/wasm/v1/tx";
 import {toUtf8} from "@cosmjs/encoding";
 import TxModal from "modules/common/components/TxModal";
 
+function addValidatorMsg(address: string, hub: string, valoper: string): EncodeObject[] {
+    const messages: EncodeObject[] = [];
+    const msg = {add_validator: {"validator": valoper}};
+    messages.push(
+        {
+            typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+            value: MsgExecuteContract.fromPartial({
+                sender: address,
+                contract: hub,
+                msg: toUtf8(JSON.stringify(msg))
+            })
+        }
+    );
+    return messages
+}
+function removeValidatorMsg(address: string, hub: string, valoper: string): EncodeObject[] {
+    const messages: EncodeObject[] = [];
+    const msg = {remove_validator: {"validator": valoper}};
+    messages.push(
+        {
+            typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+            value: MsgExecuteContract.fromPartial({
+                sender: address,
+                contract: hub,
+                msg: toUtf8(JSON.stringify(msg))
+            })
+        }
+    );
+    return messages
+}
 
 const MySteak: FC<SteakProps> = ({network, chain, client}) => {
     const {address} = useWallet();
     const {isOpen, onOpen, onClose} = useDisclosure();
     const [msgs, setMsgs] = useState<EncodeObject[]>([]);
+    const [valoper,setValOper] = useState<string>();
     if (!address) {
         return (<Header text="Please Connect"/>);
     }
     const rebalanceMsgs: EncodeObject[] = [];
     if (address && network.hub) {
-
-        // const rebalanceMsg = {rebalance: {minimum:"3000000"}}; // 3 native
         const rebalanceMsg = {rebalance: {minimum: "100000000"}}; // 100 native
         rebalanceMsgs.push(
             {
@@ -111,6 +140,44 @@ const MySteak: FC<SteakProps> = ({network, chain, client}) => {
                         isDisabled={!address}
                     >
                         Submit Batch
+                    </Button>
+                </Flex>
+                <Flex direction={["column", "row", null, null]} justify="center" mt="10">
+                    <FormLabel color='black'>Validator</FormLabel>
+                    <Input name="validator"
+                           color="black"
+                           variant="outline"
+                           placeholder="valoper...."
+                           onChange={(e)=>setValOper(e.target.value)}/>
+                    <Button
+                        type="button"
+                        variant="solid"
+                        mt="6"
+                        onClick={() => {
+                            if (valoper) {
+                                setMsgs(addValidatorMsg(address, network.hub, valoper));
+                                return onOpen()
+                            }
+                        }}
+                        isLoading={false}
+                        isDisabled={!address || !valoper || valoper==""}
+                    >
+                        Add
+                    </Button>
+                    <Button
+                        type="button"
+                        variant="solid"
+                        mt="6"
+                        onClick={() => {
+                            if (valoper) {
+                                setMsgs(removeValidatorMsg(address, network.hub, valoper));
+                                return onOpen()
+                            }
+                        }}
+                        isLoading={false}
+                        isDisabled={!address || !valoper || valoper==""}
+                    >
+                        Remove
                     </Button>
 
                 </Flex>
