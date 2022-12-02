61a8c7ebeb8af6329c148693085fd17ea8270040
Sirius Taikun <siriustaikun@protonmail.com>
2022-12-02 15:27:16 -0700

    abstracted async miner

--- hooks/useMiner.tsx
+++ hooks/useMiner.tsx
@@ -1,3 +1,7 @@
+import {
+  CosmWasmClient,
+  SigningCosmWasmClient,
+} from "@cosmjs/cosmwasm-stargate";
 import { useWallet } from "@wizard-ui/react";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import init, { mine, MinedProof } from "wasm-miner";
@@ -14,6 +18,182 @@ const localNonceStorage = {
     localStorage.setItem("nonce", nonce.toString());
   },
 };
+
+const MAX_NONCE = BigInt("18_446_744_073_709_551_615".replaceAll("_", ""));
+
+type MinerClientState = {
+  client: CosmWasmClient;
+  address: string;
+  hub: string;
+  signingClient: SigningCosmWasmClient;
+  onMinedBatch?: (proof: MinedProof) => void;
+  onMinerParams?: (params: { difficulty: bigint; entropy: string }) => void;
+};
+class Miner {
+  private clientState?: MinerClientState;
+  private nonce = BigInt(0);
+  private _nextTick: Promise<void>;
+  private start: ((...args: any[]) => void) | undefined;
+  private hasStarted = false;
+  private lastMinedProof: MinedProof | undefined;
+  minerParams: {
+    difficulty: bigint;
+    entropy: string;
+  } = { difficulty: BigInt(0), entropy: "0" };
+  constructor() {
+    this._nextTick = this.createTick();
+  }
+
+  setNonce(nonce: bigint) {
+    this.nonce = nonce;
+  }
+  setClientState(clientState: MinerClientState) {
+    this.clientState = clientState;
+  }
+
+  startMining() {
+    this.hasStarted = true;
+    this.start?.();
+  }
+
+  stopMining() {
+    this.resetNextTick();
+  }
+
+  private async *minerLoop() {
+    while (true) {
+      // await next animation frame 
+      await new Promise((resolve) => requestAnimationFrame(resolve));
+      await this.nextTick();
+      yield true;
+    }
+  }
+
+  private resetNextTick() {
+    if (!this.hasStarted) {
+      return;
+    }
+    this.hasStarted = false;
+    this._nextTick = this.createTick();
+  }
+
+  private createTick() {
+    return new Promise<void>((resolve) => {
+      this.start = resolve;
+    });
+  }
+
+  private nextTick() {
+    return this._nextTick;
+  }
+
+  private async pollForMinerParams() {
+    return new Promise<void>(async (resolve) => {
+      for await (const _ of this.minerLoop()) {
+        try {
+          if (this.clientState) {
+            const data = await this.clientState.client.queryContractSmart(
+              this.clientState.hub,
+              {
+                miner_params: {},
+              },
+            );
+            if (this.minerParams.entropy === "0") {
+              resolve(data);
+            }
+            this.minerParams = {
+              difficulty: BigInt(data.difficulty),
+              entropy: data.entropy,
+            };
+            this.clientState?.onMinerParams?.(this.minerParams);
+          }
+        } catch (e) {
+          console.error(e);
+        }
+        // wait for next block
+        await new Promise((resolve) => setTimeout(resolve, 8000));
+      }
+    });
+  }
+  runMiningBatch() {
+    if (!this.clientState) {
+      throw new Error("No client state");
+    }
+    const { nonce, success, hash } = mine(
+      this.minerParams.entropy,
+      this.clientState.address,
+      BigInt(this.minerParams.difficulty),
+      this.nonce,
+      BigInt("1000"),
+    );
+    this.lastMinedProof = {
+      nonce,
+      success,
+      hash,
+    } as MinedProof;
+    this.nonce = this.lastMinedProof.nonce + BigInt(1);
+    this.clientState?.onMinedBatch?.(this.lastMinedProof);
+    return this.lastMinedProof;
+  }
+
+  private async submitValidProof(minedProof: MinedProof) {
+    if (!minedProof.success) {
+      throw new Error("Proof is not valid");
+    }
+    if (!this.clientState) {
+      throw new Error("No client state");
+    }
+    await this.clientState.signingClient?.execute(
+      this.clientState.address,
+      this.clientState.hub,
+      {
+        submit_proof: {
+          nonce: minedProof?.nonce.toString(),
+        },
+      },
+      {
+        gas: "2000000",
+        amount: [{ denom: "ujoe", amount: "10000000" }],
+      },
+    );
+  }
+  private async pollForClientState() {
+    return new Promise<void>(async (resolve) => {
+      while (true) {
+        if (this.clientState) {
+          resolve();
+          break;
+        }
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+      }
+    });
+  }
+
+  async init() {
+    init().then(async () => {
+      console.log("initialized");
+      await this.pollForMinerParams();
+      console.log('miner params loaded')
+      for await (const _ of this.minerLoop()) {
+        if (this.nonce > MAX_NONCE) {
+          this.nonce = BigInt(0);
+        }
+        await this.pollForClientState();
+        const minedProof = this.runMiningBatch();
+        if (minedProof?.success) {
+          await this.submitValidProof(minedProof)
+            .then(console.log)
+            .catch(console.error);
+        }
+      }
+    });
+  }
+}
+
+const miner = new Miner();
+
+miner.init();
+
 export const useMiner = (hub: string) => {
   const { client, address, signingClient } = useWallet();
   const [minerStatus, setMinerStatus] = useState<"running" | "stopped">(
@@ -22,108 +202,56 @@ export const useMiner = (hub: string) => {
   const [currentMinedProof, setCurrentMinedProof] = useState<MinedProof | null>(
     null,
   );
-  const mineLoop = useMemo(
-    async function* mineLoop() {
-      if (minerStatus === "stopped") {
-        return;
-      }
-      while (true) {
-        yield true;
-        await new Promise((resolve) => requestAnimationFrame(resolve));
-      }
-    },
-    [minerStatus],
-  );
+  const [currentMinerParams, setCurrentMinerParams] = useState<{
+    entropy: string;
+    difficulty: bigint;
+  } | null>(null);
+
   const startMiner = useCallback(() => {
     setMinerStatus("running");
+    miner.startMining();
   }, []);
   const stopMiner = useCallback(() => {
     setMinerStatus("stopped");
-    mineLoop.throw("miner stopped");
+    miner.stopMining();
   }, []);
   useEffect(() => {
-    init().then(async () => {
-      for await (const _ of mineLoop) {
-        if (!client) return;
-        console.log("mining");
-        if (address === null) return;
-
-        let entropy = "0";
-        let difficulty = "1";
-
-        await new Promise(async (resolve) => {
-          for await (const _ of mineLoop) {
-            const data = await client.queryContractSmart(hub, {
-              miner_params: {},
-            });
-            if (entropy === "0") {
-              resolve(data);
-            }
-            entropy = data.entropy;
-            difficulty = data.difficulty;
-            // wait for next block
-            await new Promise((resolve) => setTimeout(resolve, 8000));
-          }
-        });
-
-        let nonce = localNonceStorage.get();
-        let minedProof: MinedProof | undefined = undefined;
-        const MAX_NONCE = BigInt(
-          "18_446_744_073_709_551_615".replaceAll("_", ""),
-        );
-        for await (const _ of mineLoop) {
-          if (nonce > MAX_NONCE) {
-            nonce = BigInt(0);
-            break;
-          }
-          minedProof = mine(
-            entropy,
-            address,
-            BigInt(difficulty),
-            nonce,
-            BigInt("100000"),
-          );
-          nonce = minedProof.nonce + BigInt(1);
-          localNonceStorage.set(nonce);
-          setCurrentMinedProof(minedProof);
-          console.log("mining...", nonce);
-        }
-        console.log("mined: ", minedProof?.nonce);
-        console.table({
-          entropy,
-          difficulty,
-          nonce: minedProof?.nonce,
-          hash: minedProof?.hash,
-        });
-
-        if (minedProof?.success) {
-          await signingClient
-            ?.execute(
-              address,
-              hub,
-              {
-                submit_proof: {
-                  nonce: minedProof?.nonce.toString(),
-                },
-              },
-              {
-                gas: "2000000",
-                amount: [{ denom: "ujoe", amount: "10000000" }],
-              },
-            )
-            .then(console.log)
-            .catch(console.error);
-        }
+    if (client && address && hub && signingClient) {
+      miner.setNonce(localNonceStorage.get())
+      if (minerStatus === "running") {
+        miner.startMining();
       }
-    });
-    return () => {
-      mineLoop.throw("miner stopped");
-    };
-  }, [minerStatus]);
+      miner.setClientState({
+        client,
+        address,
+        hub,
+        signingClient,
+        onMinedBatch: (proof) => {
+          setCurrentMinedProof(proof);
+          localNonceStorage.set(proof.nonce);
+        },
+        onMinerParams: setCurrentMinerParams,
+      });
+      return () => {
+        miner.stopMining();
+        miner.setClientState({
+          client,
+          address,
+          hub,
+          signingClient,
+          onMinedBatch: undefined,
+          onMinerParams: undefined,
+        });
+      };
+    }
+  }, [client, address, hub, signingClient]);
+
   return {
     startMiner,
     stopMiner,
     minerStatus,
     currentMinedProof,
+    currentMinerParams,
+    MAX_NONCE,
   };
 };
--- modules/steak/MySteak.tsx
+++ modules/steak/MySteak.tsx
@@ -1,4 +1,17 @@
-import { chakra, Box, Flex, Text } from "@chakra-ui/react";
+import {
+  chakra,
+  Box,
+  Flex,
+  Text,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 import { FC } from "react";
 
@@ -73,15 +86,73 @@ const MySteak: FC<SteakProps> = ({ network, chain, client }) => {
         borderRadius="2xl"
         textAlign="center"
       >
-        <Text fontSize="6xl" fontWeight="800">
-          {steakDenomBalance ? formatNumber(steakDenomBalance, 3) : "0.000"}
-        </Text>
-        <Text fontWeight="800">
-          {"($" + (steakValue ? formatNumber(steakValue, 2) : "0.00") + ")"}
-        </Text>
-        <Text color="brand.lightBrown" mt="5">
-          WETJOE balance in wallet
-        </Text>
+        <Flex
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          textAlign="center"
+          position={'relative'}
+        >
+          <Flex
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            textAlign="center"
+            opacity={miner.minerStatus == "running" ? 0 : 1}
+          >
+            <Text fontSize="6xl" fontWeight="800">
+              {steakDenomBalance ? formatNumber(steakDenomBalance, 3) : "0.000"}
+            </Text>
+            <Text fontWeight="800">
+              {"($" + (steakValue ? formatNumber(steakValue, 2) : "0.00") + ")"}
+            </Text>
+            <Text color="brand.lightBrown" mt="5">
+              WETJOE balance in wallet
+            </Text>
+          </Flex>
+          <Flex
+            opacity={miner.minerStatus == "stopped" ? 0 : 1}
+            position="absolute"
+            direction={'column'}
+            justify="center"
+            mt="10"
+            height={"100%"}
+          >
+            <video
+              preload="auto"
+              tabIndex={-1}
+              loop
+              playsInline
+              autoPlay={true}
+              muted
+              aria-label="Embedded video"
+              disablePictureInPicture
+              // poster="https://pbs.twimg.com/dm_gif_preview/1598539516503068673/mO2zAVdAhSb7XQ23Z3YFXKaLCTP0m8H8GH_QSN--gLFd4V0flu.jpg"
+              src="/pupmos-mining-animation.mp4"
+              typeof="video/mp4"
+              // style="width: 100%; height: 100%; position: absolute; background-color: black; top: 0%; left: 0%; transform: rotate(0deg) scale(1.005);"
+              style={{
+                borderRadius: "8px",
+                height: "100%",
+                backgroundColor: "black",
+                top: "0%",
+                left: "0%",
+                transform: "rotate(0deg) scale(1.005)",
+              }}
+            ></video>
+            {/* subtle credit to creator of gif */}
+            <Text
+              // position="absolute"
+              width={"100%"}
+              bottom="0"
+              right="0"
+              color="brand.lightBrown"
+              fontSize="xs"
+            >
+              gif contributed by <a target={"_blank"} href="https://twitter.com/ShutenDoji20">@ShutenDoji20</a> and <a target={"_blank"} href="https://twitter.com/MemeCaesar">@MemeCaesar</a>
+            </Text>
+          </Flex>
+        </Flex>
         <Flex
           direction={["column", "row", null, null]}
           justify="center"
@@ -114,32 +185,67 @@ const MySteak: FC<SteakProps> = ({ network, chain, client }) => {
             </chakra.a>
           )}
         </Flex>
-        <Flex>
-          {/* <StatGroup>
-            <Stat>
-              <StatLabel>Sent</StatLabel>
-              <StatNumber>345,670</StatNumber>
-              <StatHelpText>
-                <StatArrow type="increase" />
-                23.36%
-              </StatHelpText>
-            </Stat>
 
-            <Stat>
-              <StatLabel>Clicked</StatLabel>
-              <StatNumber>45</StatNumber>
-              <StatHelpText>
-                <StatArrow type="decrease" />
-                9.05%
-              </StatHelpText>
-            </Stat>
-          </StatGroup> */}
-          <Text color="brand.lightBrown" mt="5">
+        <Flex
+          direction={["column", "row", null, null]}
+          justify="center"
+          mt="10"
+        >
+          <TableContainer
+            bg="white"
+            width={"80%"}
+            textColor={"brand.red"}
+            borderRadius="2xl"
+            p="4"
+          >
+            <Table size="sm">
+              <Thead>
+                <Tr>
+                  <Th>metric</Th>
+                  <Th isNumeric>value</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>hash</Td>
+                  <Td isNumeric>
+                    {miner.currentMinedProof?.hash.substring(0, 30)}
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>nonce</Td>
+                  <Td isNumeric>
+                    {miner.currentMinedProof?.nonce.toLocaleString()}
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>entropy</Td>
+                  <Td isNumeric>
+                    {miner.currentMinerParams?.entropy.substring(0, 30)}
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>difficulty</Td>
+                  <Td isNumeric>
+                    {miner.currentMinerParams?.difficulty
+                      .toLocaleString()
+                      .padStart(30, "0")}
+                  </Td>
+                </Tr>
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th>Metric</Th>
+                  <Th isNumeric>Value</Th>
+                </Tr>
+              </Tfoot>
+            </Table>
+          </TableContainer>
+          {/* <Text color="brand.lightBrown" mt="5">
             {miner.minerStatus === "stopped"
               ? "Mining is stopped"
               : "Mining is running"}
           </Text>
-          {/* display miner minedproof */}
           <Text color="brand.lightBrown" mt="5">
             {miner.minerStatus === "stopped"
               ? ""
@@ -149,32 +255,7 @@ const MySteak: FC<SteakProps> = ({ network, chain, client }) => {
             {miner.minerStatus === "stopped"
               ? ""
               : "Nonce: " + miner.currentMinedProof?.nonce}
-          </Text>
-        </Flex>
-        <Flex justify="center" mt="10">
-          <video
-            preload="auto"
-            tabIndex={-1}
-            loop
-            playsInline
-            autoPlay={true}
-            muted
-            aria-label="Embedded video"
-            disablePictureInPicture
-            // poster="https://pbs.twimg.com/dm_gif_preview/1598539516503068673/mO2zAVdAhSb7XQ23Z3YFXKaLCTP0m8H8GH_QSN--gLFd4V0flu.jpg"
-            src="/pupmos-mining-animation.mp4"
-            typeof="video/mp4"
-            // style="width: 100%; height: 100%; position: absolute; background-color: black; top: 0%; left: 0%; transform: rotate(0deg) scale(1.005);"
-            style={{
-              borderRadius: "8px",
-              width: "80%",
-              height: "auto",
-              backgroundColor: "black",
-              top: "0%",
-              left: "0%",
-              transform: "rotate(0deg) scale(1.005)",
-            }}
-          ></video>
+          </Text> */}
         </Flex>
       </Box>
     </>
